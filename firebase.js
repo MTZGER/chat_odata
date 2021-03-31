f92357fc1aa9c3e0bdb2ea6245a2a68fac674e0c
MTZGER <66328710+MTZGER@users.noreply.github.com>
2021-03-31 14:44:03 +0200

    Updates

---
+++ firebase.js
@@ -0,0 +1,11 @@
+module.exports = () => ({
+    chats: [
+        { id: 0, title: "chat1", users: ["mtzger", "mtzger2dd"], data: ["hello1", "hello2"] },
+        { id: 1, title: "chat2", users: ["mtzger", "mtzger2s"], data: ["chat2 hello1", "chat2 hello2"] },
+        { id: 2, title: "chat3", users: ["user22323", "mtzger2"], data: ["chat3 hello1", "chat3 hello2"] },
+        { id: 4, title: "chat5", users: ["mtzger", "mtzger2sa"], data: ["chat5 hello1", "chat5 hello2"] },
+    ],
+    authUser: {
+        username: "mtzger"
+    }
+})
