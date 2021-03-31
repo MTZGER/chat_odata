7505d4c2b06e3fbe6de8b743bb78f7816355c1c8
MTZGER <66328710+MTZGER@users.noreply.github.com>
2021-03-31 14:00:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require("path")
+const firebase = require("./firebase")
+const app = express()
+const port = 3000
+
+const GET_CHAT_BY_ID = "GET_CHAT_BY_ID"
+const GET_CHATS_BY_USER = "GET_CHATS_BY_USER"
+
+const authUser = firebase().authUser
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.post('/', async (req, res) => {
+  if (req.body.type == GET_CHATS_BY_USER) {
+    const chats = firebase().chats.filter(chat => chat.users.includes(authUser.username))
+    res.send(chats)
+  } else if (req.body.type == GET_CHAT_BY_ID) {
+    const chat = firebase().chats.filter(chat => chat.id === req.body.payload)
+    res.send(chat)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
